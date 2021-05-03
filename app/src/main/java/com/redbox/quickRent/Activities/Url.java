f9e0ff8b6ce63c153962e579ffabcda77f0afd24
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-05-04 00:29:10 +0600

    Url added

---
+++ app/src/main/java/com/redbox/quickRent/Activities/Url.java
@@ -0,0 +1,9 @@
+package com.redbox.quickRent.Activities;
+
+public class Url {
+    private static final String base_url="https://local-machine.000webhostapp.com//";
+    public static  final String register_url= base_url+"register.php";
+    public static  final String login_url= base_url+"login.php";
+    public static  final String addProperty_url= base_url+"addProperty.php";
+    public static  final String retrieveProperty_url= base_url+"retrieveProperty.php";
+}
